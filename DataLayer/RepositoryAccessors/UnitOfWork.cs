8784bb0ddffa34e0a3a0b70c26ebfc610189b02b
Jonas Hielscher <benedikt.hielscher@st.ovgu.de>
2017-09-22 23:30:36 +0200

    Added a Angular 2 GUI Project.

--- DataLayer/RepositoryAccessors/UnitOfWork.cs
+++ DataLayer/RepositoryAccessors/UnitOfWork.cs
@@ -1,29 +1,49 @@
 ﻿namespace DataRepositories
 {
+    using System;
     using DatabaseModel;
     using Microsoft.EntityFrameworkCore;
+    using Repositories;
+    using Repositories.Interfaces;
 
 
     public class UnitOfWork : IUnitOfWork
     {
 
         private readonly EventMatcherModelContext context;
+        private IUserRepository userRepository;
 
         public UnitOfWork(EventMatcherModelContext context = null)
         {
             if (context == null)
             {
-                // TODO: Add connectionstring
-                var contextOptions = new DbContextOptions<EventMatcherModelContext>();
-                context = new EventMatcherModelContext(contextOptions);
+                var contextOptionBuilder = new DbContextOptionsBuilder<EventMatcherModelContext>();
+                contextOptionBuilder.UseSqlServer("");
+                context = new EventMatcherModelContext(contextOptionBuilder.Options);
             }
             this.context = context;
         }
 
+        public IUserRepository UserRepository
+        {
+            get
+            {
+                if (this.userRepository == null)
+                {
+                    this.userRepository = new UserRepository(this.context);
+                }
+                return userRepository;
+            }
+        }
+
+        public void Dispose()
+        {
+            this.context?.Dispose();
+        }
     }
 
-    public interface IUnitOfWork
+    public interface IUnitOfWork : IDisposable
     {
-
+        IUserRepository UserRepository { get; }
     }
 }
